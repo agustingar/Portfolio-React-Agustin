b4a1e987c8b358a96be1bb83972ef2fd936e1f3a
Agustín García <agustin.tavoite@gmail.com>
2023-01-25 11:52:31 +0100

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,149 @@
+import img from '../src/assets/images/AG.PNG'
+const logotext = "AGUSTÍN GARCÍA";
+const meta = {
+    title: "AGUSTIN GARCÍA",
+    description: "I’m Agustín García Frontend devloper",
+};
+
+const introdata = {
+    title: "I’m Agustín García",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop React apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: `${img}`,
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme:  "I'm passionate about learn new things, new challenges and create projects that can improve people's lives. I also like read new information for the technologies. 2023 Goals: Learn more about apps developments.Life Hack: Explore with knowledge. Fun fact: I love to play guitar and play football"
+};
+const worktimeline = [{
+    jobtitle: "Assembler Institute of Technology",
+    where: "Remote",
+    date: "2022 - 2022",
+},
+{
+    jobtitle: "Agma",
+    where: "Valencia",
+    date: "2020 - 2022",
+},
+{
+    jobtitle: "Radoo",
+    where: "Valencia",
+    date: "2020 - 2021",
+},
+];
+
+const skills = [{
+    name: "React",
+    value: 90,
+},
+{
+    name: "HTML",
+    value: 85,
+},
+{
+    name: "Wordpress",
+    value: 80,
+},
+{
+    name: "Css",
+    value: 60,
+},
+{
+    name: "Javascript",
+    value: 85,
+},
+];
+
+const services = [{
+    title: "Website Designs with Wordpress",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Programing projects",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Marketing Strategy",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/id/60/400/500/?grayscale",
+    desctiption: "Calculator with Javascript.",
+    link: "https://agustingar.github.io/calculator/",
+},
+{
+    img: 'https://picsum.photos/id/119/500/450',
+    desctiption: " Gif 4 U",
+    link: "https://challenge-agustin.vercel.app",
+},
+{
+    img: "https://picsum.photos/id/96/400/600/",
+    desctiption: "Max Clicks in 10s",
+    link: "https://vieiraraba.github.io/max-clicks-in10s/",
+},
+{
+    img: 'https://picsum.photos/id/304/400/700/',
+    desctiption: "Melody Music Stream",
+    link: "https://melody-music-stream-front.vercel.app",
+},
+
+{
+    img: "https://picsum.photos/id/6/400/400/?grayscale",
+    desctiption: "MgCollection (Wordpress)",
+    link: "https://mgcollection.es",
+},
+{
+    img: "https://picsum.photos/id/334/450/400/?grayscale",
+    desctiption: "  Spotify Clone Front",
+    link: "https://spoty-theta.vercel.app/",
+},
+
+{
+    img: "https://picsum.photos/id/445/400/550/?grayscale",
+    desctiption: "Prolink(Wordpress)",
+    link: "https://prolink.es",
+},
+{
+    img: "https://picsum.photos/id/535/400/",
+    desctiption: "MelocottonStore (Wordpress)",
+    link: "https://melocottonstore.es",
+},
+{
+    img: "https://picsum.photos/400/506?grayscale",
+    desctiption: "Portfolio",
+    link: "https://agustingarciallorca.com",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "agustin.tavoite@gmail.com",
+    YOUR_FONE: "+34 603 568 026",
+
+    YOUR_SERVICE_ID: "service_kqb0wpr",
+    YOUR_TEMPLATE_ID: "template_15l0mhe",
+    YOUR_USER_ID: "t4PpHmWZtYGQCL9LO",
+};
+
+const socialprofils = {
+    github: "https://github.com/agustingar",
+    linkedin: "https://www.linkedin.com/in/agustin-garcia-llorca-978984171/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
